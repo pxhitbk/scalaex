a0de820e03f81952cad7978e3d559b2248089e2e
pham.xuan.hung <pham.xuan.hung@vsi-international.com>
2015-01-05 12:29:22 +0700

    sample

---
+++ src/main/scala/com/study/scala/scalaex/App.scala
@@ -0,0 +1,9 @@
+package com.study.scala.scalaex
+
+/**
+ * Hello world!
+ *
+ */
+object App extends App {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/com/study/scala/scalaex/AppTest.scala
@@ -0,0 +1,17 @@
+package com.study.scala.scalaex
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
