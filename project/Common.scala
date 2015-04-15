2cb824db25cd968acd406cd7f21f9b6939ccee3c
Hung Pham Xuan <hungpx3@fsoft.com.vn>
2015-04-15 18:25:07 +0700

    add scalaz library

--- project/Common.scala
+++ project/Common.scala
@@ -10,6 +10,7 @@ object Common {
   val libraryDependencies = Seq(
     "joda-time" % "joda-time" % "2.7",
     "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.12.2" % "test"
+    "org.scalacheck" %% "scalacheck" % "1.12.2" % "test",
+    "org.scalaz" %% "scalaz-core" % "7.1.1"
   )
 }
