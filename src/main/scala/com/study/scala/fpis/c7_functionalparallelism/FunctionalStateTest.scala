478ab106d44f31f43f4e65df00a4f668de37a5d3
Pham Xuan Hung <hungpx3@fsoft.com.vn>
2015-06-18 00:38:32 +0700

    random example

--- src/main/scala/com/study/scala/fpis/c7_functionalparallelism/FunctionalStateTest.scala
+++ src/main/scala/com/study/scala/fpis/c7_functionalparallelism/FunctionalStateTest.scala
@@ -4,29 +4,57 @@ package com.study.scala.fpis.c7_functionalparallelism
  * Created by g10-macmini on 6/17/15.
  */
 object FunctionalStateTest extends App {
-  println(RGN.simple(1))
-  println(RGN.simple(1))
-  println(RGN.simple(1))
+  println(RNG.simple(1))
+  println(RNG.simple(1))
+  println(RNG.simple(1))
 }
 
-trait RGN {
-  def nextInt(): (Int, RGN)
-
-  def positiveInt(rgn: RGN): (Int, RGN)
-}
-
-object RGN {
-  type Rand[+A] = RGN => (A, RGN)
+trait RNG {
+  /**
+   * General-purpose function describe state actions that transform RNG states,
+   * and these state actions can be built up and combined.
+   * */
+  type Rand[+A] = RNG => (A, RNG) //Type alias for a function.
 
   val int: Rand[Int] = _.nextInt()
 
-  def simple(seed: Long): RGN = new RGN {
+  def unit[A](a: A): Rand[A] = self => (a, self)
+
+  /**
+   * Transforming the output of a state action without modifying the state itself
+   * @param s
+   * @param f
+   * @tparam A
+   * @tparam B
+   * @return
+   */
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = { rng =>
+    val (b, rng2) = s(rng)
+    (f(b), rng2)
+  }
+
+  def positiveMax(n: Int): Rand[Int] = { self =>
+    def gen(rNG: RNG): (Int, RNG) = {
+      val r = positiveInt(rNG)
+      if (r._1 < n) r
+      else gen(r._2)
+    }
+    gen(self)
+  }
+
+  def nextInt(): (Int, RNG)
+
+  def positiveInt(rgn: RNG): (Int, RNG)
+}
+
+object RNG {
+  def simple(seed: Long): RNG = new RNG {
     def nextInt() = {
       val seed2 = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
       ((seed2 >>> 16).asInstanceOf[Int], simple(seed2))
     }
 
-    override def positiveInt(rgn: RGN): (Int, RGN) = {
+    override def positiveInt(rgn: RNG): (Int, RNG) = {
       val (i, r) = rgn.nextInt()
       (Math.abs(i), r)
     }
